bd68b4e09ba3443bb0e1a90d4efe6321696b20d6
amigacheva <anutamig@mail.ru>
2019-09-30 21:29:09 +0300

    init commit

---
+++ easy.py
@@ -0,0 +1,13 @@
+#!/usr/bin/lessons
+# -*- coding: utf-8 -*-
+# задача 1
+x = input("x:")
+a = str(x)
+counter = 0
+print (a)
+index = len(a) - 1
+while counter <= index:
+    print (a[counter])
+    counter += 1
+print ("finished")
+# test commit
