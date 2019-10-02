e496bd6085eeb7b80cdcf526f1df94294df0b0d4
AKovalchuk <anutamig@mail.ru>
2019-10-02 23:26:06 +0300

    refactor

--- normal2.1.py
+++ normal2.1.py
@@ -34,7 +34,7 @@ two = int(input("two: "))
 one = one*two
 two = one/two
 one = one/two
-print ('one = ', int(one), 'two = ', int(two))
+print('one = ', int(one), 'two = ', int(two))
 
 # Задача-3: Напишите программу, вычисляющую корни квадратного уравнения вида
 # ax² + bx + c = 0.
@@ -47,11 +47,11 @@ b = int(input("b"))
 c = int(input("c"))
 d= (b*b)-4*(a*c)
 if d<0:
-    print ("нет решения")
+    print("нет решения")
 elif d==0:
     x0=(-b)/2*a
     print ("есть один корень", x0)
 else:
     x1 = (-b + math.sqrt(d))/(2*a)
     x2 = (-b - math.sqrt(d))/(2*a)
-    print ('x1=', x1, 'x2=', x2)
+    print('x1=', x1, 'x2=', x2)
