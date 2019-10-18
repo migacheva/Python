e79ba0c7a6327a87a37381a1819434ba3e067567
AKovalchuk <anutamig@mail.ru>
2019-10-18 21:00:12 +0300

    Создание и удаление директорий

--- easy2.5.py
+++ easy2.5.py
@@ -10,28 +10,27 @@ print("Текущая рабочая директория %s" % path)
 
 def create_dirs(name):
     # определим имя директории, которую создаём
-    path = os.getcwd() + "/" + name
-    # for i in range(10):
-    #     path = os.getcwd() + "/" + name + "/1"
-    #     os.mkdirs(path)
-    try:
-        os.mkdir(path)
-    except OSError:
-        print ("Создать директорию %s не удалось" % path)
-    else:
-        print ("Успешно создана директория %s " % path)
+    for i in range(1,10):
+        path = os.getcwd() + "\\" + name + str(i)
+        try:
+            os.mkdir(path)
+        except OSError:
+            print ("Создать директорию %s не удалось" % path)
+        else:
+            print ("Успешно создана директория %s " % path)
 
 def delete_dirs(name):
-    path = os.getcwd() + "/" + name
-    try:
-        os.rmdir(path)
-    except OSError:
-        print ("Удалить директорию %s не удалось" % path)
-    else:
-        print ("Успешно удалена директория %s " % path)
+    for i in range(1,10):
+        path = os.getcwd() + "\\" + name + str(i)
+        try:
+            os.rmdir(path)
+        except OSError:
+            print ("Удалить директорию %s не удалось" % path)
+        else:
+            print ("Успешно удалена директория %s " % path)
 
-create_dirs("newf")
-delete_dirs("newf")
+create_dirs("dir")
+delete_dirs("dir")
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
