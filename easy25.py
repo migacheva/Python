d11e21b547f1a62cfa5543abf71acb53a691255a
AKovalchuk <anutamig@mail.ru>
2019-10-20 21:14:41 +0300

    Копирование файла в новуюу директорию

--- easy25.py
+++ easy25.py
@@ -1,4 +1,6 @@
 import os
+from shutil import copyfile,copy
+
 # Задача-1:
 # Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
 # из которой запущен данный скрипт.
@@ -8,50 +10,63 @@ import os
 path = os.getcwd()
 print("Текущая рабочая директория %s" % path)
 
+
 def create_dirs9(name):
     # определим имя директории, которую создаём
-    for i in range(1,10):
+    for i in range(1, 10):
         path = os.getcwd() + "\\" + name + str(i)
         try:
             os.mkdir(path)
         except OSError:
-            print ("Создать директорию %s не удалось" % path)
+            print("Создать директорию %s не удалось" % path)
         else:
-            print ("Успешно создана директория %s " % path)
+            print("Успешно создана директория %s " % path)
+
 
 def create_dirs(name):
-
     path = os.getcwd() + "\\" + name
     try:
         os.mkdir(path)
     except OSError:
-        print ("Создать директорию %s не удалось" % path)
+        print("Создать директорию %s не удалось" % path)
     else:
-        print ("Успешно создана директория %s " % path)
+        print("Успешно создана директория %s " % path)
+
 
 def delete_dirs9(name):
-    for i in range(1,10):
+    for i in range(1, 10):
         path = os.getcwd() + "\\" + name + str(i)
         try:
             os.rmdir(path)
         except OSError:
-            print ("Удалить директорию %s не удалось" % path)
+            print("Удалить директорию %s не удалось" % path)
         else:
-            print ("Успешно удалена директория %s " % path)
+            print("Успешно удалена директория %s " % path)
+
 
 def delete_dirs(name):
     path = os.getcwd() + "\\" + name
     try:
         os.rmdir(path)
     except OSError:
-        print ("Удалить директорию %s не удалось" % path)
+        print("Удалить директорию %s не удалось" % path)
     else:
-        print ("Успешно удалена директория %s " % path)
+        print("Успешно удалена директория %s " % path)
+
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 def list_dir():
     print(os.listdir())
 
+
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+# Не рабочий вариант
+def copyfile():
+    destdir = os.path.abspath('destdir')
+    if not os.path.exists(destdir):
+        os.makedirs(destdir)
+    copy(__file__, destdir)  # копируем в указанную директорию
+
+copyfile()
